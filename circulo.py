3cb87e2fd74f50e2aad4bdb9be0107140d18c5e1
Ian McIntosh <61014227+Crian53@users.noreply.github.com>
2022-04-27 21:03:43 -0300

    Create circulo.py

---
+++ circulo.py
@@ -0,0 +1,14 @@
+void setup()
+{
+  size(600,600);
+  rectMode(CENTER);
+}
+
+void draw()
+{
+  background(200);
+  translate(width/2,height/2);
+  fill(255,255,0);
+  noStroke();
+  circle(0,0,100);
+}
